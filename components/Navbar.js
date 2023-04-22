223df0cee349731af09c2d6e258b9cbcfebb4521
tarunnjoshi <tarun12joshi@gmail.com>
2023-04-22 14:25:31 +0530

    added pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,20 +5,22 @@ import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 const Navbar = () => {
   return (
-    <div className='flex flex-col md:flex-row md:jus justify-center items-center py-2'>
+    <div className='flex flex-col md:flex-row md:justify-start justify-center items-center mb-1 shadow-xl'>
       <div className="logo mx-5">
+        <Link href={'/'}>
         <Image src="/navlogo.png" alt="" width={127} height={40} />
+        </Link>
       </div>
       <div className="nav">
-        <ul className='flex items-center space-x-2 font-bold md:text-xl'>
-          <Link href={'/'}> <li>Tshirt</li> </Link>
-          <Link href={'/'}><li>Hoodies</li></Link>
-          <Link href={'/'}> <li>Snikers</li> </Link>
-          <Link href={'/'}> <li>Mugs</li> </Link>
+        <ul className='flex items-center space-x-6 font-bold md:text-md'>
+          <Link href={'/tshirts'}> <li>Tshirt</li> </Link>
+          <Link href={'/hoodies'}><li>Hoodies</li></Link>
+          <Link href={'/stickers'}> <li>Stickers</li> </Link>
+          <Link href={'/mugs'}> <li>Mugs</li> </Link>
         </ul>
       </div>
       <div className="cart absolute right-0 top-4 mx-5">
-        <AiOutlineShoppingCart className='text-xl md:text-3xl'></AiOutlineShoppingCart>
+        <AiOutlineShoppingCart className='text-xl md:text-2xl'></AiOutlineShoppingCart>
       </div>
     </div>
   )
--- pages/mugs.js
+++ pages/mugs.js
@@ -1,8 +1,215 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 
 const Mugs = () => {
   return (
-    <div>Mugs</div>
+<div>
+  <section className="text-gray-600 body-font">
+    <div className="container px-5 py-24 mx-auto">
+      <div className="flex flex-wrap -m-4">
+        <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg lg:mr-0.75">
+          <a  href={'/product/wear-the-code'} className="block relative rounded overflow-hidden">
+            <img alt="ecommerce" className="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/71hbvqJtzxS._SL1500_.jpg"/>
+          <div className="mt-4">
+            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3> 
+            <h2 className="text-gray-900 title-font text-lg font-medium">EverGreen E-commerce</h2>
+            <p className="mt-1">₹550.00</p>
+            <p className="mt-1">S, M, L, XL, XX</p>
+          </div>
+          </a>
+        </div>
+        <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg lg:mr-0.75">
+          <a  href={'/product/wear-the-code'} className="block relative rounded overflow-hidden">
+            <img alt="ecommerce" className="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/71hbvqJtzxS._SL1500_.jpg"/>
+          <div className="mt-4">
+            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+            <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+            <p className="mt-1">₹21.15</p>
+            <p className="mt-1">S, M, L, XL, XX</p>
+          </div>
+          </a>
+        </div>
+        <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg lg:mr-0.75">
+          <a  href={'/product/wear-the-code'} className="block relative rounded overflow-hidden">
+            <img alt="ecommerce" className="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/71hbvqJtzxS._SL1500_.jpg"/>
+          <div className="mt-4">
+            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+            <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+            <p className="mt-1">₹12.00</p>
+            <p className="mt-1">S, M, L, XL, XX</p>
+          </div>
+          </a>
+        </div>
+        <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg lg:mr-0.75">
+          <a  href={'/product/wear-the-code'} className="block relative rounded overflow-hidden">
+            <img alt="ecommerce" className="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/71hbvqJtzxS._SL1500_.jpg"/>
+          <div className="mt-4">
+            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+            <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+            <p className="mt-1">₹18.40</p>
+            <p className="mt-1">S, M, L, XL, XX</p>
+          </div>
+          </a>
+        </div>
+        <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg lg:mr-0.75">
+          <a  href={'/product/wear-the-code'} className="block relative rounded overflow-hidden">
+            <img alt="ecommerce" className="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/71hbvqJtzxS._SL1500_.jpg"/>
+          <div className="mt-4">
+            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+            <h2 className="text-gray-900 title-font text-lg font-medium">EverGreen E-commerce</h2>
+            <p className="mt-1">₹550.00</p>
+            <p className="mt-1">S, M, L, XL, XX</p>
+          </div>
+          </a>
+        </div>
+        <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg lg:mr-0.75">
+          <a  href={'/product/wear-the-code'} className="block relative rounded overflow-hidden">
+            <img alt="ecommerce" className="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/71hbvqJtzxS._SL1500_.jpg"/>
+          <div className="mt-4">
+            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+            <h2 className="text-gray-900 title-font text-lg font-medium">EverGreen E-commerce</h2>
+            <p className="mt-1">₹550.00</p>
+            <p className="mt-1">S, M, L, XL, XX</p>
+          </div>
+          </a>
+        </div>
+        <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg lg:mr-0.75">
+          <a  href={'/product/wear-the-code'} className="block relative rounded overflow-hidden">
+            <img alt="ecommerce" className="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/71hbvqJtzxS._SL1500_.jpg"/>
+          <div className="mt-4">
+            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+            <h2 className="text-gray-900 title-font text-lg font-medium">EverGreen E-commerce</h2>
+            <p className="mt-1">₹550.00</p>
+            <p className="mt-1">S, M, L, XL, XX</p>
+          </div>
+          </a>
+        </div>
+        <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg lg:mr-0.75">
+          <a  href={'/product/wear-the-code'} className="block relative rounded overflow-hidden">
+            <img alt="ecommerce" className="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/71hbvqJtzxS._SL1500_.jpg"/>
+          <div className="mt-4">
+            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+            <h2 className="text-gray-900 title-font text-lg font-medium">EverGreen E-commerce</h2>
+            <p className="mt-1">₹550.00</p>
+            <p className="mt-1">S, M, L, XL, XX</p>
+          </div>
+          </a>
+        </div>
+        <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg lg:mr-0.75">
+          <a  href={'/product/wear-the-code'} className="block relative rounded overflow-hidden">
+            <img alt="ecommerce" className="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/71hbvqJtzxS._SL1500_.jpg"/>
+          <div className="mt-4">
+            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+            <h2 className="text-gray-900 title-font text-lg font-medium">EverGreen E-commerce</h2>
+            <p className="mt-1">₹550.00</p>
+            <p className="mt-1">S, M, L, XL, XX</p>
+          </div>
+          </a>
+        </div>
+        <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg lg:mr-0.75">
+          <a  href={'/product/wear-the-code'} className="block relative rounded overflow-hidden">
+            <img alt="ecommerce" className="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/71hbvqJtzxS._SL1500_.jpg"/>
+          <div className="mt-4">
+            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+            <h2 className="text-gray-900 title-font text-lg font-medium">EverGreen E-commerce</h2>
+            <p className="mt-1">₹550.00</p>
+            <p className="mt-1">S, M, L, XL, XX</p>
+          </div>
+          </a>
+        </div>
+        <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg lg:mr-0.75">
+          <a  href={'/product/wear-the-code'} className="block relative rounded overflow-hidden">
+            <img alt="ecommerce" className="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/71hbvqJtzxS._SL1500_.jpg"/>
+          <div className="mt-4">
+            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+            <h2 className="text-gray-900 title-font text-lg font-medium">EverGreen E-commerce</h2>
+            <p className="mt-1">₹550.00</p>
+            <p className="mt-1">S, M, L, XL, XX</p>
+          </div>
+          </a>
+        </div>
+        <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg lg:mr-0.75">
+          <a  href={'/product/wear-the-code'} className="block relative rounded overflow-hidden">
+            <img alt="ecommerce" className="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/71hbvqJtzxS._SL1500_.jpg"/>
+          <div className="mt-4">
+            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+            <h2 className="text-gray-900 title-font text-lg font-medium">EverGreen E-commerce</h2>
+            <p className="mt-1">₹550.00</p>
+            <p className="mt-1">S, M, L, XL, XX</p>
+          </div>
+          </a>
+        </div>
+        <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg lg:mr-0.75">
+          <a  href={'/product/wear-the-code'} className="block relative rounded overflow-hidden">
+            <img alt="ecommerce" className="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/71hbvqJtzxS._SL1500_.jpg"/>
+          <div className="mt-4">
+            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+            <h2 className="text-gray-900 title-font text-lg font-medium">EverGreen E-commerce</h2>
+            <p className="mt-1">₹550.00</p>
+            <p className="mt-1">S, M, L, XL, XX</p>
+          </div>
+          </a>
+        </div>
+        <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg lg:mr-0.75">
+          <a  href={'/product/wear-the-code'} className="block relative rounded overflow-hidden">
+            <img alt="ecommerce" className="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/71hbvqJtzxS._SL1500_.jpg"/>
+          <div className="mt-4">
+            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+            <h2 className="text-gray-900 title-font text-lg font-medium">EverGreen E-commerce</h2>
+            <p className="mt-1">₹550.00</p>
+            <p className="mt-1">S, M, L, XL, XX</p>
+          </div>
+          </a>
+        </div>
+        <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg lg:mr-0.75">
+          <a  href={'/product/wear-the-code'} className="block relative rounded overflow-hidden">
+            <img alt="ecommerce" className="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/71hbvqJtzxS._SL1500_.jpg"/>
+          <div className="mt-4">
+            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+            <h2 className="text-gray-900 title-font text-lg font-medium">EverGreen E-commerce</h2>
+            <p className="mt-1">₹550.00</p>
+            <p className="mt-1">S, M, L, XL, XX</p>
+          </div>
+          </a>
+        </div>
+        <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg lg:mr-0.75">
+          <a  href={'/product/wear-the-code'} className="block relative rounded overflow-hidden">
+            <img alt="ecommerce" className="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/71hbvqJtzxS._SL1500_.jpg"/>
+          <div className="mt-4">
+            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+            <h2 className="text-gray-900 title-font text-lg font-medium">EverGreen E-commerce</h2>
+            <p className="mt-1">₹550.00</p>
+            <p className="mt-1">S, M, L, XL, XX</p>
+          </div>
+          </a>
+        </div>
+        <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg lg:mr-0.75">
+          <a  href={'/product/wear-the-code'} className="block relative rounded overflow-hidden">
+            <img alt="ecommerce" className="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/71hbvqJtzxS._SL1500_.jpg"/>
+          <div className="mt-4">
+            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+            <h2 className="text-gray-900 title-font text-lg font-medium">EverGreen E-commerce</h2>
+            <p className="mt-1">₹550.00</p>
+            <p className="mt-1">S, M, L, XL, XX</p>
+          </div>
+          </a>
+        </div>
+        <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg lg:mr-0.75">
+          <a  href={'/product/wear-the-code'} className="block relative rounded overflow-hidden">
+            <img alt="ecommerce" className="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/71hbvqJtzxS._SL1500_.jpg"/>
+          <div className="mt-4">
+            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+            <h2 className="text-gray-900 title-font text-lg font-medium">EverGreen E-commerce</h2>
+            <p className="mt-1">₹550.00</p>
+            <p className="mt-1">S, M, L, XL, XX</p>
+          </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</div>
   )
 }
 
