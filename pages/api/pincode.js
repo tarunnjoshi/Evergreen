7252580099687d4ee46c2a3f582b7f90e9d1408d
tarunnjoshi <tarun12joshi@gmail.com>
2023-04-25 08:48:55 +0530

    making product page and ad pincde service

---
+++ pages/api/pincode.js
@@ -0,0 +1,6 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+    res.status(200).json([263601, 12346, 111111])
+  }
+  
