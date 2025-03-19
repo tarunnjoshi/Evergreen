ed64dff2c89e969a36143cbc23f285530f3d24f1
tarunnjoshi <tarun12joshi@gmail.com>
2025-03-19 14:04:42 +0530

    fix the order file

--- models/Order.js
+++ models/Order.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const OrderSchema = new mongoose.Schema({
-    userId: {tyoe: String, required: true},
+    userId: {type: String, required: true},
     products: [
         {
             productId: {type: String},
